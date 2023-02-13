23dd49e880d918176e2029fb4ef4ba3403d40a8a
Thomas shyam <81607554+thomasshyam@users.noreply.github.com>
2023-02-13 12:09:32 +0530

    Add files via upload

---
+++ Notifications.js
@@ -0,0 +1,40 @@
+export default Notifications = [
+    {
+        id : 1,
+        category : 'Bakery',
+        productName: 'Bread',
+        Qty: 1,
+        Unit: '100g',
+        price: '30'
+    },
+    {
+        id : 2,
+        productName : 'Chocolate',
+        Qty:2,
+        Unit: '500g',
+        price: '50'
+    },
+    {
+        id: 3,
+        category : 'Soap',
+        productName: 'Dove Soap',
+        Qty:2,
+        Unit: '250g',
+        price: '10'
+    },
+    {
+        id:4,
+        productName: 'Pears',
+        Qty:4,
+        Unit: '250g',
+        price: '20'
+    },
+    {
+        id:5,
+        category : 'Jam',
+        productName: 'Kissan Jam',
+        Qty:1,
+        Unit: '500g',
+        price: '30'
+    },
+];
